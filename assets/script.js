59bed80ca7f8112503b9c44a84b54b1ad9d51dbd
TimL94 <99834249+TimL94@users.noreply.github.com>
2024-02-05 19:13:35 -0500

    added icons for the weather

--- assets/script.js
+++ assets/script.js
@@ -13,46 +13,46 @@ searchButton.addEventListener('click', function(){
     })
     .then(function (data) {
         console.log(data);
-
+        var iconPre = 'https://openweathermap.org/img/wn/';
+        var iconSuf = '@2x.png';
         $('.custom-city').each(function() {
-            $(this).text($('#search-text').val())
+            $(this).text(data.city.name)
         })
+
         $('#box-1-1').text(data.list[0].dt_txt.split(' ')[0]);
         $('#box-1-2').text(data.list[0].weather[0].description);
-        $('#box-1-3').text(data.list[0].weather[0].icon);
+        $('#img-1').attr('src', iconPre + data.list[0].weather[0].icon + iconSuf);
         $('#box-1-4').text(data.list[0].main.temp + ' F');
         $('#box-1-5').text('Humidity: ' + data.list[0].main.humidity);
-        $('#box-1-6').text('Wind Speed: ' + data.list[0].wind.speed + ' MPH');
 
+        $('#box-1-6').text('Wind Speed: ' + data.list[0].wind.speed + ' MPH');
         $('#box-2-1').text(data.list[8].dt_txt.split(' ')[0]);
         $('#box-2-2').text(data.list[8].weather[0].description);
-        $('#box-2-3').text(data.list[8].weather[0].icon);
+        $('#img-2').attr('src', iconPre + data.list[8].weather[0].icon + iconSuf);
         $('#box-2-4').text(data.list[8].main.temp + ' F');
         $('#box-2-5').text('Humidity: ' + data.list[8].main.humidity);
         $('#box-2-6').text('Wind Speed: ' + data.list[8].wind.speed + ' MPH');
 
         $('#box-3-1').text(data.list[16].dt_txt.split(' ')[0]);
         $('#box-3-2').text(data.list[16].weather[0].description);
-        $('#box-3-3').text(data.list[16].weather[0].icon);
+        $('#img-3').attr('src', iconPre + data.list[16].weather[0].icon + iconSuf);
         $('#box-3-4').text(data.list[16].main.temp + ' F');
         $('#box-3-5').text('Humidity: ' + data.list[16].main.humidity);
         $('#box-3-6').text('Wind Speed: ' + data.list[16].wind.speed + ' MPH');
 
         $('#box-4-1').text(data.list[31].dt_txt.split(' ')[0]);
         $('#box-4-2').text(data.list[31].weather[0].description);
-        $('#box-4-3').text(data.list[31].weather[0].icon);
+        $('#img-4').attr('src', iconPre + data.list[31].weather[0].icon + iconSuf);
         $('#box-4-4').text(data.list[31].main.temp + ' F');
         $('#box-4-5').text('Humidity: ' + data.list[32].main.humidity);
         $('#box-4-6').text('Wind Speed: ' + data.list[32].wind.speed + ' MPH');
 
         $('#box-5-1').text(data.list[39].dt_txt.split(' ')[0]);
         $('#box-5-2').text(data.list[39].weather[0].description);
-        $('#box-5-3').text(data.list[39].weather[0].icon);
+        $('#img-5').attr('src', iconPre + data.list[39].weather[0].icon + iconSuf);
         $('#box-5-4').text(data.list[39].main.temp + ' F');
         $('#box-5-5').text('Humidity: ' + data.list[39].main.humidity);
         $('#box-5-6').text('Wind Speed: ' + data.list[39].wind.speed + ' MPH');
-
-
 
     })
 });
